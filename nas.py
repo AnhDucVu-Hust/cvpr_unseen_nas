672017bad23224e0bf083de9006ce3d65ac1494a
AnhDucVu <88773938+AnhDucVu-Hust@users.noreply.github.com>
2023-05-31 01:19:04 +0700

    Update nas.py

--- nas.py
+++ nas.py
@@ -201,7 +201,7 @@ class NAS:
                 logging.info('train %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
 
         return top1.avg, objs.avg
-    def infer(valid_queue, model, criterion):
+    def infer(self,valid_queue, model, criterion):
         objs = utils.AvgrageMeter()
         top1 = utils.AvgrageMeter()
         top5 = utils.AvgrageMeter()
