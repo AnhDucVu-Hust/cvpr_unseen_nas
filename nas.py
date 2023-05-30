8a4e7f2a6e1e6f7a82f1a5317a20d149bb78dcca
AnhDucVu <88773938+AnhDucVu-Hust@users.noreply.github.com>
2023-05-30 22:30:15 +0700

    Update nas.py

--- nas.py
+++ nas.py
@@ -201,3 +201,26 @@ class NAS:
                 logging.info('train %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
 
         return top1.avg, objs.avg
+    def infer(valid_queue, model, criterion):
+        objs = utils.AvgrageMeter()
+        top1 = utils.AvgrageMeter()
+        top5 = utils.AvgrageMeter()
+        model.eval()
+
+        for step, (input, target) in enumerate(valid_queue):
+
+            input = Variable(input, volatile=True).cuda()
+            target = Variable(target, volatile=True).cuda()
+            logits = model(input)
+            loss = criterion(logits, target)
+
+            prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
+            n = input.size(0)
+            objs.update(loss.item(), n)
+            top1.update(prec1.item(), n)
+            top5.update(prec5.item(), n)
+
+            if step % args.report_freq == 0:
+              logging.info('valid %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
+
+          return top1.avg, objs.avg
