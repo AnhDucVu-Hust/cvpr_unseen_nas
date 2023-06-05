c4aa98c093a06d7418019d62bf08693d0d865e80
tomatofarmer <89388548+smogemonkey@users.noreply.github.com>
2023-06-05 21:29:23 +0700

    Update data_processor.py

--- data_processor.py
+++ data_processor.py
@@ -15,9 +15,9 @@ class Nas_Data(Dataset):
         img = self.image[item]
         h, w = img.shape
         if height %2 != 0:
-            F.pad(input = img, pad = (0, 0, 0, 1), mode='constant', value=0)
+            self.image[item] = F.pad(input = img, pad = (0, 0, 0, 1), mode='constant', value=0)
         if width % 2 == 0:
-            F.pad(input = img, pad = (1, 0, 0, 0), mode='constant', value=0)
+            self.image[item] = F.pad(input = img, pad = (1, 0, 0, 0), mode='constant', value=0)
         if self.test != True:
             return torch.from_numpy(self.image[item]).float(),torch.tensor(self.label[item])
         else:
