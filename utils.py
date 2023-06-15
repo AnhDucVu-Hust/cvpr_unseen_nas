022c9ce707f0dd63a648ac2cd7aa01a0d3eef56c
AnhDucVu <88773938+AnhDucVu-Hust@users.noreply.github.com>
2023-06-15 15:27:36 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -46,7 +46,7 @@ class Cutout(object):
 
 
 
-def accuracy(self, output, target, topk=(1,)):
+def accuracy(output, target, topk=(1,)):
     maxk = max(topk)
     batch_size = target.size(0)
 
@@ -62,23 +62,23 @@ def accuracy(self, output, target, topk=(1,)):
 
 
 
-def count_parameters_in_MB(self, model):
+def count_parameters_in_MB(model):
     return np.sum(np.prod(v.size()) for name, v in model.named_parameters() if "auxiliary" not in name) / 1e6
 
-def save_checkpoint(self, state, is_best, save):
+def save_checkpoint(state, is_best, save):
     filename = os.path.join(save, 'checkpoint.pth.tar')
     torch.save(state, filename)
     if is_best:
         best_filename = os.path.join(save, 'model_best.pth.tar')
         shutil.copyfile(filename, best_filename)
 
-def save(self, model, model_path):
+def save(model, model_path):
     torch.save(model.state_dict(), model_path)
 
-def load(self, model, model_path):
+def load(model, model_path):
     model.load_state_dict(torch.load(model_path))
 
-def drop_path(self, x, drop_prob):
+def drop_path(x, drop_prob):
     if drop_prob > 0.:
         keep_prob = 1. - drop_prob
         mask = Variable(torch.cuda.FloatTensor(x.size(0), 1, 1, 1).bernoulli_(keep_prob))
@@ -86,7 +86,7 @@ def drop_path(self, x, drop_prob):
         x.mul_(mask)
     return x
 
-def create_exp_dir(self, path, scripts_to_save=None):
+def create_exp_dir(path, scripts_to_save=None):
     if not os.path.exists(path):
         os.mkdir(path)
     print('Experiment dir : {}'.format(path))
